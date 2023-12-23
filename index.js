bd95b4fc0073df5b382a349c07eaf9e47c547b3a
mdhanifbiswas27 <mdhanifbiswas27@gmail.com>
2023-12-23 09:01:19 +0600

    2 api created for post reviw and load review item

--- index.js
+++ index.js
@@ -28,11 +28,13 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const productsCollection = client.db('productDB').collection('products');
     const shopUserCollection =client.db('productDB').collection('shopUser');
-    const cartCollection =client.db('productDB').collection('userCart')
+    const cartCollection =client.db('productDB').collection('userCart');
+    const customerReviewsCollection = client.db('productDB').collection('customerReviews');
+
 // To get all product at once time
  app.get('/products', async(req, res)=>{
     const cursor = productsCollection.find();
@@ -45,6 +47,15 @@ app.get('/userCart', async(req, res)=>{
   const result = await cursor.toArray();
   res.send(result);
 })
+
+// get all customer reviews
+app.get('/customerReviews', async(req,res)=>{
+  const cursor = customerReviewsCollection.find();
+  const result = await cursor.toArray();
+  res.send(result);
+})
+
+
 //  get product on id
 app.get('/products/:id',async(req, res)=>{
   const id =req.params.id
@@ -89,6 +100,14 @@ app.put('/products/:id', async(req, res)=>{
      const result = await productsCollection.insertOne(newProduct);
      res.send(result);
  })
+
+
+//  add a customer review
+app.post('/customerReviews', async(req, res)=>{
+  const NewReview = req.body;
+  const result = await customerReviewsCollection.insertOne(NewReview)
+  res.send(result);
+})
 
 //  add user
 app.post('/shopUser', async(req, res)=>{
